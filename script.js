d0e9fb6fb5279f0a2c60925a5fbd00e6d24d97d5
No kredits <ekobili37@gmail.com>
2024-12-09 10:55:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const Eshop = [
+  { name: "E-shop" },
+  { phoneNumber: 87769856463 },
+  (products = [
+    {
+      productName: "Iphone 14",
+      category: "Электроника",
+      price: 500000,
+      stock: 20,
+    },
+    {
+      productName: "Apple Watch",
+      category: "Электроника",
+      price: 200000,
+      stock: 50,
+    },
+    { productName: "Lays", category: "Тамақ", price: 800, stock: 40 },
+    { productName: "Cola", category: "Тамақ", price: 500, stock: 45 },
+    { productName: "Tennis", category: "Oйынғa", price: 2500, stock: 12 },
+    { productName: "Ball", category: "Oйынғa", price: 5000, stock: 27 },
+  ]),
+];
+
